7f73682ad1ce6e973f33fa079dbdef52231e8fed
Quin-shi-huan <phelipee06@gmail.com>
2025-05-09 20:46:15 -0300

    feat: Implementing validators dir with pydantic

--- src/views/fisic_person_creator_view.py
+++ src/views/fisic_person_creator_view.py
@@ -1,3 +1,4 @@
+from src.validators.fisic_person_creator_validator import fisic_person_creator_validator
 from .http_types.http_request import HttpRequest
 from .http_types.http_response import HttpResponse
 from ..controllers.fisic_person_creator_controller import FisicPersonCreatorController
@@ -8,7 +9,7 @@ class FisicPersonCreatorView(ViewInterface):
         self.__controller = controller
 
     def handle(self, http_request: HttpRequest) -> HttpResponse:
-
+        fisic_person_creator_validator(http_request)
         person_info = http_request.body
         body_response = self.__controller.create(person_info)
 
--- src/views/juridic_person_creator_view.py
+++ src/views/juridic_person_creator_view.py
@@ -1,3 +1,4 @@
+from src.validators.juridic_person_creator_validator import juridic_person_creator_validator
 from .http_types.http_request import HttpRequest
 from .http_types.http_response import HttpResponse
 from ..controllers.juridic_person_creator_controller import JuridicPersonCreatorController
@@ -8,7 +9,7 @@ class JuridicPersonCreatorView(ViewInterface):
         self.__controller = controller
 
     def handle(self, http_request: HttpRequest) -> HttpResponse:
-
+        juridic_person_creator_validator(http_request)
         person_info = http_request.body
         body_response = self.__controller.create(person_info)
 
